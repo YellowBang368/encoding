35dabb6769619ef662199a761837372b2e81655d
TopClone <yellowbang368@gmail.com>
2017-10-25 17:29:15 +0300

    Added SHA2 encoding

--- coding.rb
+++ coding.rb
@@ -3,9 +3,11 @@ require "digest"
 puts "Как зашифровать?"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 choice = STDIN.gets.chomp
 puts "Что зашифровать?"
 word = STDIN.gets.chomp
 
 puts Digest::MD5.hexdigest(word) if choice == "1"
 puts Digest::SHA1.hexdigest(word) if choice == "2"
+puts Digest::SHA2.hexdigest(word) if choice == "3"
